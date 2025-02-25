539af8c0811c3647e56c6412522c01b3e0dcd2d0
栗功哲 <23371506@buaa.edu.cn>
2025-02-25 21:56:50 +0800

    第一次尝试

---
+++ csc/code/modify.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sed -i 's/char/int/g' fibo.c
--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
-#Then you can use awk to separate the answer.
+#Then you can use awk to separate the answer
+grep -n int file >> result
+sed -i 's/:int a=1//g' result

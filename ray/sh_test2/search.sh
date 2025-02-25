3a6d655d1b8c1842fc96401bf5dce6397fbeb6ce
栗功哲 <23371506@buaa.edu.cn>
2025-02-25 22:25:24 +0800

    2

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer
-grep -n int file >> result
-sed -i 's/:int a=1//g' result
+grep -n $2 $1 >> $3
+sed -i 's/:int a=1//g' $3

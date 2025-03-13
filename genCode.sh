7ae75821537773d3ab31fcc0686acb17962663aa
栗功哲 <23371506@buaa.edu.cn>
2025-03-13 20:32:20 +0800

    second

--- genCode.sh
+++ genCode.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 mkdir codeSet
-sed -i '1i\#include"include/libsy.h"' *.sy
-sed -i 's\getInt\getint\g' *.sy
-cp *.sy codeSet/*.c
+sed -i '1i\#include"include/libsy.h"' code/wa.sy
+sed -i 's/getInt/getint/g' code/wa.sy
+cp code/wa.sy codeSet/wa.c
+i=0
+for((i=1;i<=10;i++)); do
+	sed -i '1i\#include"include/libsy.sh"' code/code${i}.sy
+	sed -i 's/getInt/getint/g' code/code${i}.sy
+	cp code/${i}.sy codeSet/code${i}.c
+done

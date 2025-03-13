e48d1223fa37c25fcd4121ec090d72518764df8b
栗功哲 <23371506@buaa.edu.cn>
2025-03-13 20:05:19 +0800

    sixth

--- exam_9.sh
+++ exam_9.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+c=0
+d=0
 a=$1
 b=$2
-if [ $a -eq 0 -a $b -eq 0 ]
+((c=a))
+((d=b))
+if [ $c -eq 0 -a $d -eq 0 ]
 then
 	cat stderr.txt
-elif [ $a -gt 0 -a $b -eq 0]
+elif [ $c -gt 0 -a $d -eq 0 ]
 then
-	awk 'NR>=${a} {print $0}' stderr.txt
+	awk 'NR>=${c} {print $0}' stderr.txt
 else
-	awk 'NR>=${a}&&NR<${b} {print $0}' stderr.txt
+	awk 'NR>=${c}&&NR<${d} {print $0}' stderr.txt
 fi

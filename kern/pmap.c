a6d3736cb3ce7685540da07f19784b54bd2a3535
栗功哲 <23371506@buaa.edu.cn>
2025-04-10 19:55:04 +0800

    1

--- kern/pmap.c
+++ kern/pmap.c
@@ -353,14 +353,13 @@ void page_remove(Pde *pgdir, u_int asid, u_long va) {
 
 u_int page_conditional_remove(Pde *pgdir, u_int asid, u_int perm_mask, u_long begin_va, u_long end_va) {
 	u_int count = 0;
-	u_int va;
+	u_long va;
 	Pte *pte;
 	struct Page *pp;
 	for (va = begin_va;va<end_va;va+=PAGE_SIZE) {
 		if (page_lookup(pgdir, va, &pte) != NULL) {
 			pp = pa2page(*pte);
-			u_int mask = PTE_FLAGS(pte);
-			if (perm_mask & mask) {
+			if ((perm_mask & *pte)) {
 				page_decref(pp);
 				*pte = 0;
 				tlb_invalidate(asid, va);
